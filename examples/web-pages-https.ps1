98e8df1b7c15a2b6f57d0193a368c1ec0b5a6cbe
Matthew Kelly <badgerati42@gmail.com>
2019-04-03 21:32:15 +0100

    #217: tweaks to example after testing

--- examples/web-pages-https.ps1
+++ examples/web-pages-https.ps1
@@ -10,14 +10,16 @@ Import-Module "$($path)/src/Pode.psm1" -Force -ErrorAction Stop
 # This will not clear the binding afterwards (netsh http delete sslcert 0.0.0.0:8443), nor will it remove the certificate
 # from the personal store.  Cleanup should be done manually as required. Generated self-signed cert for fqdn localhost,
 # this is just for dev/testing and proof of concept
-
-$port = 8443
+#
+# to use the hostname listener, you'll need to add "pode.foo.com  127.0.0.1" to your hosts file
+# ----------------------------------
 
 # create a server, flagged to generate a self-signed cert for dev/testing
 Server {
 
     # bind to ip/port and set as https with self-signed cert
-    listen *:$port https -cert self
+    listen *:8443 https -cert self
+    #listen "pode.foo.com:8443" https -cert self
 
     # set view engine for web pages
     engine pode
