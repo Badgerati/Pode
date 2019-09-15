de8691bee41205baca7fc49757efb0c4f4e09b5b
Matthew Kelly <badgerati42@gmail.com>
2019-09-15 11:59:09 +0100

    #376: fix the content-type/length not be sent back

--- src/Private/PodeServer.ps1
+++ src/Private/PodeServer.ps1
@@ -97,37 +97,7 @@ function Start-PodeSocketServer
                     $bytes = New-Object byte[] 8192
                     $bytesRead = (Wait-PodeTask -Task $stream.ReadAsync($bytes, 0, 8192))
                     $req_msg = $encoder.GetString($bytes, 0, $bytesRead)
-
-                    # parse the request headers
-                    $req_lines = ($req_msg -isplit [System.Environment]::NewLine)
-
-                    # first line is the request info
-                    $req_line_info = ($req_lines[0] -isplit '\s+')
-                    $req_method = $req_line_info[0]
-                    $req_query = $req_line_info[1]
-                    $req_proto = $req_line_info[2]
-
-                    # then, read the headers
-                    $req_headers = @{}
-                    $req_body_index = 0
-                    for ($i = 1; $i -le $req_lines.Length -1; $i++) {
-                        $line = $req_lines[$i]
-                        if ([string]::IsNullOrWhiteSpace($line)) {
-                            $req_body_index = $i + 1
-                            break
-                        }
-
-                        $index = $line.IndexOf(':')
-                        $name = $line.Substring(0, $index).Trim()
-                        $value = $line.Substring($index + 1).Trim()
-                        $req_headers[$name] = $value
-                    }
-
-                    # then set the request body
-                    $req_body = ($req_lines[($req_body_index)..($req_lines.Length - 1)] -join [System.Environment]::NewLine)
-
-                    # build required URI details
-                    $req_uri = [uri]::new("$($context.Protocol)://$($req_headers['Host'])$($req_query)")
+                    $req_info = Get-PodeServerRequestDetails -Content $req_msg -Protocol $context.Protocol
 
                     # reset the event data
                     $WebEvent = @{
@@ -142,24 +112,24 @@ function Start-PodeSocketServer
                             StatusDescription = 'OK'
                         }
                         Request = @{
-                            RawBody = $req_body
-                            Headers = $req_headers
-                            Url = $req_uri
-                            UrlReferrer = $req_headers['Referer']
-                            UserAgent = $req_headers['User-Agent']
-                            HttpMethod = $req_method
+                            RawBody = $req_info.Body
+                            Headers = $req_info.Headers
+                            Url = $req_info.Uri
+                            UrlReferrer = $req_info.Headers['Referer']
+                            UserAgent = $req_info.Headers['User-Agent']
+                            HttpMethod = $req_info.Method
                             RemoteEndPoint = $context.Socket.RemoteEndPoint
-                            Protocol = $req_proto
-                            ProtocolVersion = ($req_proto -isplit '/')[1]
-                            ContentEncoding = (Get-PodeEncodingFromContentType -ContentType $req_headers['Content-Type'])
+                            Protocol = $req_info.Protocol
+                            ProtocolVersion = ($req_info.Protocol -isplit '/')[1]
+                            ContentEncoding = (Get-PodeEncodingFromContentType -ContentType $req_info.Headers['Content-Type'])
                         }
                         Lockable = $PodeContext.Lockable
-                        Path = $req_uri.AbsolutePath
-                        Method = $req_method.ToLowerInvariant()
-                        Query = [System.Web.HttpUtility]::ParseQueryString($req_uri.Query)
+                        Path = $req_info.Uri.AbsolutePath
+                        Method = $req_info.Method.ToLowerInvariant()
+                        Query = [System.Web.HttpUtility]::ParseQueryString($req_info.Query)
                         Protocol = $context.Protocol
-                        Endpoint = $req_headers['Host']
-                        ContentType = $req_headers['Content-Type']
+                        Endpoint = $req_info.Headers['Host']
+                        ContentType = $req_info.Headers['Content-Type']
                         ErrorType = $null
                         Cookies = $null
                         PendingCookies = @{}
@@ -187,7 +157,23 @@ function Start-PodeSocketServer
                     }
 
                     # write the response line
-                    $res_msg = "$($req_proto) $($WebEvent.Response.StatusCode) $($WebEvent.Response.StatusDescription)$([Environment]::NewLine)"
+                    $res_msg = "$($req_info.Protocol) $($WebEvent.Response.StatusCode) $($WebEvent.Response.StatusDescription)$([Environment]::NewLine)"
+
+                    # add content-type
+                    if (![string]::IsNullOrWhiteSpace($WebEvent.Response.ContentType)) {
+                        Set-PodeHeader -Name 'Content-Type' -Value $WebEvent.Response.ContentType
+                    }
+                    else {
+                        $WebEvent.Response.Headers.Remove('Content-Type')
+                    }
+
+                    # add content-length
+                    if ($WebEvent.Response.ContentLength64 -gt 0) {
+                        Set-PodeHeader -Name 'Content-Length' -Value $WebEvent.Response.ContentLength64
+                    }
+                    else {
+                        $WebEvent.Response.Headers.Remove('Content-Length')
+                    }
 
                     # write the response headers
                     if ($WebEvent.Response.Headers.Count -gt 0) {
@@ -264,5 +250,59 @@ function Start-PodeSocketServer
     # browse to the first endpoint, if flagged
     if ($Browse) {
         Start-Process $endpoints[0].HostName
+    }
+}
+
+function Get-PodeServerRequestDetails
+{
+    param(
+        [Parameter()]
+        [string]
+        $Content,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $Protocol
+    )
+
+    # parse the request headers
+    $req_lines = ($Content -isplit [System.Environment]::NewLine)
+
+    # first line is the request info
+    $req_line_info = ($req_lines[0] -isplit '\s+')
+    $req_method = $req_line_info[0]
+    $req_query = $req_line_info[1]
+    $req_proto = $req_line_info[2]
+
+    # then, read the headers
+    $req_headers = @{}
+    $req_body_index = 0
+    for ($i = 1; $i -le $req_lines.Length -1; $i++) {
+        $line = $req_lines[$i]
+        if ([string]::IsNullOrWhiteSpace($line)) {
+            $req_body_index = $i + 1
+            break
+        }
+
+        $index = $line.IndexOf(':')
+        $name = $line.Substring(0, $index).Trim()
+        $value = $line.Substring($index + 1).Trim()
+        $req_headers[$name] = $value
+    }
+
+    # then set the request body
+    $req_body = ($req_lines[($req_body_index)..($req_lines.Length - 1)] -join [System.Environment]::NewLine)
+
+    # build required URI details
+    $req_uri = [uri]::new("$($Protocol)://$($req_headers['Host'])$($req_query)")
+
+    # return the details
+    return @{
+        Method = $req_method
+        Query = $req_query
+        Protocol = $req_proto
+        Headers = $req_headers
+        Body = $req_body
+        Uri = $req_uri
     }
 }
