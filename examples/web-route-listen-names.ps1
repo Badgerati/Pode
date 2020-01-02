731ae92be838504506a392c35e4b8ca9f68d8a24
Matthew Kelly <badgerati42@gmail.com>
2020-01-02 20:22:31 +0000

    #448: support on routes for multiple EndpointNames

--- examples/web-route-listen-names.ps1
+++ examples/web-route-listen-names.ps1
@@ -10,18 +10,25 @@ Start-PodeServer {
     # listen on localhost:8080/8443
     Add-PodeEndpoint -Address 127.0.0.1 -Port 8080 -Protocol Http -Name 'local1'
     Add-PodeEndpoint -Address 127.0.0.2 -Port 8080 -Protocol Http -Name 'local2'
+    Add-PodeEndpoint -Address 127.0.0.3 -Port 8080 -Protocol Http -Name 'local3'
+    Add-PodeEndpoint -Address 127.0.0.4 -Port 8080 -Protocol Http -Name 'local4'
 
     # set view engine to pode
     Set-PodeViewEngine -Type Pode
 
-    # GET request for web page
+    # GET request for web page - all endpoints
     Add-PodeRoute -Method Get -Path '/' -ScriptBlock {
         Write-PodeViewResponse -Path 'simple' -Data @{ 'numbers' = @(1, 2, 3); }
     }
 
-    # GET request for web page, but only for the local2 endpoint
+    # GET request for web page -  local2 endpoint
     Add-PodeRoute -Method Get -Path '/' -EndpointName 'local2' -ScriptBlock {
         Write-PodeViewResponse -Path 'simple' -Data @{ 'numbers' = @(1, 2, 3, 4, 5, 6, 7, 8); }
+    }
+
+    # GET request for web page -  local3 and local4 endpoints
+    Add-PodeRoute -Method Get -Path '/' -EndpointName 'local3', 'local4' -ScriptBlock {
+        Write-PodeViewResponse -Path 'simple' -Data @{ 'numbers' = @(2, 4, 6, 8, 10, 12, 14, 16); }
     }
 
     # GET request to download a file
