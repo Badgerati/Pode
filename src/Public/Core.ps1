dcad5c59c2e5a18aa73a37d3c68cd1cde23251a7
Matthew Kelly <badgerati42@gmail.com>
2021-03-24 17:30:57 +0000

    #717: fix looping service closing immediately

--- src/Public/Core.ps1
+++ src/Public/Core.ps1
@@ -164,7 +164,7 @@ function Start-PodeServer
         Start-PodeInternalServer -Request $Request -Browse:$Browse
 
         # at this point, if it's just a one-one off script, return
-        if (($PodeContext.Server.Types.Length -eq 0) -or $PodeContext.Server.IsServerless) {
+        if (!$PodeContext.Server.IsService -and (($PodeContext.Server.Types.Length -eq 0) -or $PodeContext.Server.IsServerless)) {
             return
         }
 
