786541a97dc7a4e096d8b74f3075d71bd5744944
CruzMarcio <40072316+CruzMarcio@users.noreply.github.com>
2021-10-21 13:01:58 -0300

    Update Authentication.ps1

--- src/Private/Authentication.ps1
+++ src/Private/Authentication.ps1
@@ -186,7 +186,7 @@ function Get-PodeOAuth2RedirectHost
     )
 
     if ($RedirectUrl.StartsWith('/')) {
-        if ($PodeContext.Server.IsIIS -or $PodeContext.Server.IsHeroku) {
+        if ($PodeContext.Server.IsIIS -or $PodeContext.Server.IsHeroku -or $PodeContext.Server.IsCF) {
             $protocol = Get-PodeHeader -Name 'X-Forwarded-Proto'
             if ([string]::IsNullOrWhiteSpace($protocol)) {
                 $protocol = 'https'
