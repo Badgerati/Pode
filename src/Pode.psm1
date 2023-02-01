43d6e00d01cb9b1ffdb0fa3e5516ac27fb1f8e02
Matthew Kelly <badgerati42@gmail.com>
2023-02-01 19:42:51 +0000

    #1046: clean-up of code leftover while testing

--- src/Pode.psm1
+++ src/Pode.psm1
@@ -2,29 +2,20 @@
 $root = Split-Path -Parent -Path $MyInvocation.MyCommand.Path
 
 # load assemblies
-$assemblies = [System.AppDomain]::CurrentDomain.GetAssemblies().Location
+Add-Type -AssemblyName System.Web
+Add-Type -AssemblyName System.Net.Http
 
-if (($assemblies -ilike '*System.Web.dll*').Length -eq 0) {
-    Add-Type -AssemblyName System.Web
+# netstandard2 for <7.2
+if ($PSVersionTable.PSVersion -lt [version]'7.2.0') {
+    Add-Type -LiteralPath "$($root)/Libs/netstandard2.0/Pode.dll" -ErrorAction Stop
 }
-
-if (($assemblies -ilike '*System.Net.Http.dll*').Length -eq 0) {
-    Add-Type -AssemblyName System.Net.Http
+# net6 for =7.2
+elseif ($PSVersionTable.PSVersion -lt [version]'7.3.0') {
+    Add-Type -LiteralPath "$($root)/Libs/net6.0/Pode.dll" -ErrorAction Stop
 }
-
-if (($assemblies -ilike '*Pode.dll*').Length -eq 0) {
-    # netstandard2 for <7.2
-    if ($PSVersionTable.PSVersion -lt [version]'7.2.0') {
-        Add-Type -LiteralPath "$($root)/Libs/netstandard2.0/Pode.dll" -ErrorAction Stop
-    }
-    # net6 for =7.2
-    elseif ($PSVersionTable.PSVersion -lt [version]'7.3.0') {
-        Add-Type -LiteralPath "$($root)/Libs/net6.0/Pode.dll" -ErrorAction Stop
-    }
-    # net7 for >7.2
-    else {
-        Add-Type -LiteralPath "$($root)/Libs/net7.0/Pode.dll" -ErrorAction Stop
-    }
+# net7 for >7.2
+else {
+    Add-Type -LiteralPath "$($root)/Libs/net7.0/Pode.dll" -ErrorAction Stop
 }
 
 # load private functions
