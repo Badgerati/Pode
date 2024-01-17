76acd9acf1750bf7f7bb1598af699059d8414c3a
mdaneri <mdaneri@vmware.com>
2024-01-17 15:51:20 -0800

    new static web example

---
+++ examples/SwaggerEditor/swagger-editor.ps1
@@ -0,0 +1,29 @@
+param(
+    [int]
+    $Port = 8085
+)
+
+$path = Split-Path -Parent -Path (Split-Path -Parent -Path (Split-Path -Parent -Path $MyInvocation.MyCommand.Path))
+Import-Module "$($path)/src/Pode.psm1" -Force -ErrorAction Stop
+
+# or just:
+# Import-Module Pode
+
+# create a server, and start listening on port 8085
+Start-PodeServer -Threads 2 {
+
+    # listen on localhost:8085
+    Add-PodeEndpoint -Address localhost -Port $port -Protocol Http
+    New-PodeLoggingMethod -Terminal | Enable-PodeRequestLogging
+    New-PodeLoggingMethod -Terminal | Enable-PodeErrorLogging
+
+    # set view engine to pode renderer
+    Set-PodeViewEngine -Type HTML
+
+    # STATIC asset folder route
+    Add-PodeStaticRoute -Path '/editor' -Source './www' -Defaults @('index.html')
+    Add-PodeStaticRoute -Path '/edtor/swagger-editor-dist' -Source './swagger-editor-dist'
+    Add-PodeRoute -Method Get -Path '/' -ScriptBlock {
+        Move-PodeResponseUrl -Url '/editor'
+    }
+}
