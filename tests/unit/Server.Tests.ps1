c37364a29b1f87fb9651cdcf818105a8bc7b24fd
mdaneri <mdaneri@vmware.com>
2024-03-26 15:54:01 -0700

    restore Server.test.ps1

--- tests/unit/Server.Tests.ps1
+++ tests/unit/Server.Tests.ps1
@@ -15,7 +15,7 @@ BeforeAll {
 
 Describe 'Start-PodeInternalServer' {
     BeforeAll {
-        Mock Add-PodePSInbuiltDrives { }
+        Mock Add-PodePSInbuiltDrive { }
         Mock Invoke-PodeScriptBlock { }
         Mock New-PodeRunspaceState { }
         Mock New-PodeRunspacePools { }
