0eed342e77c345719a4683b68ad4b84a7c5e396a
mdaneri <max.daneri@broadcom.com>
2024-07-10 21:06:37 -0700

    new dutch language resources

--- tests/unit/Localization.Tests.ps1
+++ tests/unit/Localization.Tests.ps1
@@ -8,10 +8,10 @@ BeforeDiscovery {
     $localizationDir = "$src/Locales"
 
     # Discover all language directories
-    $languageDirs = Get-ChildItem -Path $localizationDir -Directory | Where-Object { $_.Name -ne 'en' }
+    $languageDirs = (Get-ChildItem -Path $localizationDir -Directory | Where-Object { $_.Name -ne 'en' }).FullName
 
     # Get all source code files recursively from the specified directory
-    $sourceFiles = Get-ChildItem -Path $src -Recurse -Include *.ps1, *.psm1
+    $sourceFiles = (Get-ChildItem -Path $src -Recurse -Include *.ps1, *.psm1).FullName
     Import-LocalizedData -BindingVariable LanguageOfReference -BaseDirectory $localizationDir -FileName 'Pode' -UICulture 'en'
 }
 
@@ -47,32 +47,32 @@ Describe 'Localization Check' {
         return $keys
     }
 
-    Describe 'Verify Invalid Hashtable Keys in [<_.Name>]' -ForEach  ($sourceFiles) {
-        $keysInFile = Export-KeysFromFile -filePath $_.FullName
+    Describe 'Verify Invalid Hashtable Keys in [<_>]' -ForEach  ($sourceFiles) {
+        $keysInFile = Export-KeysFromFile -filePath $_
         It "should find the key '[<_>]' in the hashtable"  -ForEach  ($keysInFile) {
             $PodeLocale.Keys -contains $_ | Should -BeTrue
         }
     }
 
 
-    It 'Check `throw` is not using a static string in [<_.Name>]' -ForEach  ($sourceFiles) {
-        ( Get-Content -Path $_.FullName -Raw) -match 'throw\s*["\'']' | Should -BeFalse
+    It 'Check `throw` is not using a static string in [<_>]' -ForEach  ($sourceFiles) {
+        ( Get-Content -Path $_ -Raw) -match 'throw\s*["\'']' | Should -BeFalse
     }
 
-    Describe  'Verifying Language [<_.Name>]' -ForEach  ($languageDirs) {
+    Describe  'Verifying Language [<_>]' -ForEach  ($languageDirs) {
 
         BeforeAll {
             $content = Import-LocalizedData -FileName 'Pode.psd1' -BaseDirectory $localizationDir -UICulture $_.Name
         }
         it 'Language resource file exist' {
-            Test-Path -Path "$($_.FullName)/Pode.psd1" | Should -BeTrue
+            Test-Path -Path "$($_)/Pode.psd1" | Should -BeTrue
         }
 
         it 'Number of entry equal to the [en]' {
             $content.Keys.Count | Should -be $PodeLocale.Count
         }
 
-        It -ForEach ( $LanguageOfReference.Keys) -Name 'Resource File contains <_>' {
+        It  -Name 'Resource File contains <_>' -ForEach ( $LanguageOfReference.Keys) {
             $content.Keys -contains $_ | Should -BeTrue
         }
     }
