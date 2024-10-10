96bf571fee461be280ff42ede0297d5a4b30a639
mdaneri <max.daneri@broadcom.com>
2024-10-09 18:52:27 -0700

    fix the bidirectional pipeline

--- src/Public/Watchdog.ps1
+++ src/Public/Watchdog.ps1
@@ -101,6 +101,8 @@ function Enable-PodeWatchdog {
                 catch {
                     $_ | Write-PodeErrorLog
                     write-podehost "Error reading from pipe: $_"
+                    $pipeServer.Disconnect()
+                    Start-Sleep -Seconds 1
                 }
             }
         }
@@ -109,7 +111,7 @@ function Enable-PodeWatchdog {
             Write-PodeHost 'Cleaning up resources...'
             $reader.Dispose()
             # Disconnect the pipe server to reset it for the next client
-            $pipeServer.Disconnect()
+
         }
 
     }
@@ -239,13 +241,13 @@ function Stop-PodeWatchdog {
         switch ($PodeContext.Server.Watchdog.Type) {
             'Client' {
                 write-podehost 'Stopping Client watchdog'
-                Start-PodeWatchdogHearthbeat
+                Stop-PodeWatchdogHearthbeat
 
             }
             'Server' {
                 write-podehost 'Stopping Server watchdog'
-                Open-PodeWatchdogRunPool
-                $null = Start-PodeWatchdogProcess
+
+                $null = Stop-PodeWatchdogProcess
             }
         }
     }
@@ -257,18 +259,82 @@ function Stop-PodeWatchdog {
 
 function Start-PodeWatchdogHearthbeat {
     write-podehost  $PodeContext.Server.Watchdog -explode
+
+
+
     if ($PodeContext.Server.containsKey('Watchdog') -and $PodeContext.Server.Watchdog.Type -eq 'Client') {
         # Create a named pipe client and connect to the server
         try {
-            $PodeContext.Server.Watchdog.PipeClient = [System.IO.Pipes.NamedPipeClientStream]::new('.', $PodeContext.Server.Watchdog.PipeName, [System.IO.Pipes.PipeDirection]::InOut, [System.IO.Pipes.PipeOptions]::Asynchronous)
+            $PodeContext.Server.Watchdog['PipeClient'] = [System.IO.Pipes.NamedPipeClientStream]::new('.',
+                $PodeContext.Server.Watchdog.PipeName, [System.IO.Pipes.PipeDirection]::InOut, [System.IO.Pipes.PipeOptions]::Asynchronous)
             # Attempt to connect to the server with a timeout
             $PodeContext.Server.Watchdog.PipeClient.Connect(60000)  # Timeout in milliseconds (60 seconds)
             Write-PodeHost 'Connected to the watchdog pipe server.'
 
             # Create a persistent StreamWriter for writing messages and store it in the context
-            $PodeContext.Server.Watchdog.PipeWriter = [System.IO.StreamWriter]::new($PodeContext.Server.Watchdog.PipeClient)
-            $PodeContext.Server.Watchdog.PipeWriter.AutoFlush = $true  # Enable auto-flush to send data immediately
-            $PodeContext.Server.Watchdog.PipeReader = [System.IO.StreamReader]::new($PodeContext.Server.Watchdog.PipeClient)
+            $PodeContext.Server.Watchdog['PipeWriter'] = [System.IO.StreamWriter]::new($PodeContext.Server.Watchdog.PipeClient)
+            $PodeContext.Server.Watchdog['PipeWriter'].AutoFlush = $true  # Enable auto-flush to send data immediately
+
+
+
+
+            $scriptBlock = {
+                Write-PodeHost 'Start client receiver'
+                $PodeContext.Server.Watchdog['PipeReader'] = [System.IO.StreamReader]::new($PodeContext.Server.Watchdog.PipeClient)
+                $readTask = $null
+                while ($true) {
+                    try {
+                        if ($null -eq $readTask) {
+                            # Use ReadLineAsync() to asynchronously read data from the server without blocking
+                            $readTask = $PodeContext.Server.Watchdog.PipeReader.ReadLineAsync()
+                        }
+
+                        # Wait for the read task to complete with a timeout to avoid indefinite blocking
+                     #   $timeout = [System.Threading.Tasks.Task]::Delay(10000)  # 10 seconds timeout
+                  #     [System.Threading.Tasks.Task]::WhenAny($readTask, $timeout) | Out-Null
+
+                        if ($readTask.Status -eq [System.Threading.Tasks.TaskStatus]::RanToCompletion -and $readTask.Result) {
+                            $serverMessage = $readTask.Result
+
+                            if ($serverMessage) {
+                                Write-PodeHost "Received command from server: $serverMessage"
+
+                                # Handle server commands
+                                switch ($serverMessage) {
+                                    'shutdown' {
+                                        Write-PodeHost 'Server requested shutdown. Closing client...'
+                                        Close-PodeServer
+                                        break  # Exit the loop and stop the client
+                                    }
+                                    'restart' {
+                                        Write-PodeHost 'Server requested restart. Restarting client...'
+                                        Restart-PodeServer  # Restart the watchdog
+                                        break
+                                    }
+                                    default {
+                                        Write-PodeHost "Unknown command received: $serverMessage"
+                                    }
+                                }
+                            }
+                        }
+                        elseif ($readTask.Status -eq [System.Threading.Tasks.TaskStatus]::Faulted) {
+                            Write-PodeHost "Read operation faulted: $($readTask.Exception.Message)"
+                        }
+                        else {
+
+                          #  Write-PodeHost 'Read operation timed out or was not completed successfully.'
+                        }
+                        Start-Sleep -Seconds 1
+                    }
+                    catch {
+                        Write-PodeHost "Error reading command from server: $_"
+                        Start-Sleep -Seconds 1  # Sleep for a second before retrying in case of an error
+                    }
+                }
+            }
+            Open-PodeWatchdogRunPool
+
+            $PodeContext.Server.Watchdog['Runspace'] = Add-PodeRunspace -Type 'Watchdog' -ScriptBlock ( $scriptBlock) -PassThru
 
             Add-PodeTimer -Name '__pode_watchdog_client__' -Interval $PodeContext.Server.Watchdog.Interval -OnStart -ScriptBlock {
 
@@ -308,37 +374,7 @@ function Start-PodeWatchdogHearthbeat {
                     Close-PodeServer
                 }
 
-                # Non-blocking server message listener inside the timer block
-                try {
-                    # Attempt to read a line from the server with a small timeout (adjust as needed)
-                    if ($PodeContext.Server.Watchdog.PipeReader.Peek() -ne -1) {
-                        $serverMessage = $PodeContext.Server.Watchdog.PipeReader.ReadLine()
 
-                        if ($serverMessage) {
-                            Write-PodeHost "Received command from server: $serverMessage"
-
-                            # Handle server commands
-                            switch ($serverMessage) {
-                                'shutdown' {
-                                    Write-PodeHost 'Server requested shutdown. Closing client...'
-                                    Close-PodeServer
-                                    break  # Exit the loop and stop the client
-                                }
-                                'restart' {
-                                    Write-PodeHost 'Server requested restart. Restarting client...'
-                                    Restart-PodeServer # Restart the watchdog
-                                    break
-                                }
-                                default {
-                                    Write-PodeHost "Unknown command received: $serverMessage"
-                                }
-                            }
-                        }
-                    }
-                }
-                catch {
-                    Write-PodeHost "Error reading command from server: $_"
-                }
             }
         }
         catch [TimeoutException] {
@@ -454,7 +490,7 @@ function Restart-PodeWatchdogProcess {
             $PodeContext.Server.Watchdog.PipeWriter.WriteLine('restart')
 
             # Manually flush the StreamWriter to ensure the data is sent immediately
-            $PodeContext.Server.Watchdog.PipeWriter.Flush()
+             $PodeContext.Server.Watchdog.PipeWriter.Flush()
 
             $PodeContext.Server.Watchdog.Status = 'Restarting'
         }
