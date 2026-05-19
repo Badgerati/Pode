62a2eeba228a63c37ee29ee9b87d3d3029e2e1a6
Cédric Belin <cedx@outlook.com>
2026-05-19 11:06:37 +0200

    #1708: Fix for default folders whose custom path is not recognized

--- src/Public/Core.ps1
+++ src/Public/Core.ps1
@@ -948,6 +948,7 @@ function Set-PodeDefaultFolder {
     )
     if (Test-Path -Path $Path -PathType Container) {
         $PodeContext.Server.DefaultFolders[$Type] = $Path
+        $PodeContext.Server.InbuiltDrives[$Type] = (New-PodePSDrive -Path $path)
     }
     else {
         # Path does not exist
